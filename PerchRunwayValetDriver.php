886c6dcf5bc2adf841e06e16220615db5ec161f1
Grant Smith <grant@doddle.agency>
2024-03-06 13:10:43 +0000

    Update PerchRunwayValetDriver.php
    Fixed static path issue

--- PerchRunwayValetDriver.php
+++ PerchRunwayValetDriver.php
@@ -6,7 +6,11 @@ use Valet\Drivers\ValetDriver;
 
 class PerchRunwayValetDriver extends ValetDriver
 {
-    private $folders = ['admin', 'build', 'control', 'perch', 'public', 'site_admin',];
+    // Array of possible folders for Perch Runway. Change as needed.
+    private $folders = ['admin', 'build', 'control', 'perch', 'public', 'site_admin'];
+    
+    // Configuration variable for the static files path. Change as needed.
+    private const STATIC_FILES_PATH = '/dist/';
 
     /**
      * Determine if the driver serves the request.
@@ -35,23 +39,22 @@ class PerchRunwayValetDriver extends ValetDriver
      * @param string $uri
      * @return string
      */
-	public function frontControllerPath(string $sitePath, string $siteName, string $uri): string
-	{
-		$_SERVER['PHP_SELF'] = $uri;
-		$_SERVER['SERVER_ADDR'] = '127.0.0.1';
-		$_SERVER['SERVER_NAME'] = $_SERVER['HTTP_HOST'];
+    public function frontControllerPath(string $sitePath, string $siteName, string $uri): string
+    {
+        $_SERVER['PHP_SELF'] = $uri;
+        $_SERVER['SERVER_ADDR'] = '127.0.0.1';
+        $_SERVER['SERVER_NAME'] = $_SERVER['HTTP_HOST'];
 
-		$folder = $this->getFolder($sitePath);
+        $folder = $this->getFolder($sitePath);
 
-		// Determine the correct path based on the presence of the folder in the URI
-		if (strpos($uri, $folder) !== false) {
-			// If the folder is in the URI, use the default front controller path
-			return $sitePath . '/' . $folder . '/core/runway/start.php';
-		}
+        // If the URI starts with '/control/', serve the Perch Runway application
+        if (strpos($uri, '/control/') === 0) {
+            return $sitePath . '/control/core/runway/start.php';
+        }
 
-		// If the folder is not in the URI, use the default front controller path
-		return $sitePath . '/' . $folder . '/core/runway/start.php';
-	}
+        // If the URI doesn't start with '/control/', try to serve the start.php file
+        return $sitePath . '/' . $folder . '/core/runway/start.php';
+    }
 
     /**
      * Determine if the incoming request is for a static file.
@@ -63,15 +66,16 @@ class PerchRunwayValetDriver extends ValetDriver
      */
     public function isStaticFile(string $sitePath, string $siteName, string $uri)
     {
-        $staticFilePath = $sitePath . '/dist/' . $uri;
-
-        if (file_exists($staticFilePath)) {
-            return $staticFilePath;
+        // Check if the URI starts with the static files path
+        if (strpos($uri, self::STATIC_FILES_PATH) === 0) {
+            $staticFilePath = $sitePath . $uri;
+            if (is_file($staticFilePath)) {
+                return $staticFilePath;
+            }
         }
 
         return false;
     }
-
 
     /**
      * Get active folder of project
